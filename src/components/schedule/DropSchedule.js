cb204739453f5ed07cd0f449296e3b0aa69bd9a8
Chris Steffes <c.steffes@live.ca>
2020-12-10 08:27:49 -0600

    Pretty Schedule

--- src/components/schedule/DropSchedule.js
+++ src/components/schedule/DropSchedule.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { Row, Form } from "react-bootstrap"
+import { Row, Form, Table } from "react-bootstrap"
 
 function DropSchedule() {
   // REGEX filter to parse schedule
@@ -20,12 +20,55 @@ function DropSchedule() {
 
     if (scheduleItems) {
       scheduleItems.forEach((item, index) => {
-        console.log(item)
-        items.push(
-          <tr key={index}>
-            <td>{item}</td>
-          </tr>
-        )
+        if (item.includes("Monday")) {
+          items.push(
+            <tr className="bg-info" key={index}>
+              <th>{item}</th>
+            </tr>
+          )
+        } else if (item.includes("Tuesday")) {
+          items.push(
+            <tr className="bg-info" key={index}>
+              <th>{item}</th>
+            </tr>
+          )
+        } else if (item.includes("Wednesday")) {
+          items.push(
+            <tr className="bg-info" key={index}>
+              <th>{item}</th>
+            </tr>
+          )
+        } else if (item.includes("Thursday")) {
+          items.push(
+            <tr className="bg-info" key={index}>
+              <th>{item}</th>
+            </tr>
+          )
+        } else if (item.includes("Friday")) {
+          items.push(
+            <tr className="bg-info" key={index}>
+              <th>{item}</th>
+            </tr>
+          )
+        } else if (item.includes("Saturday")) {
+          items.push(
+            <tr className="bg-info" key={index}>
+              <th>{item}</th>
+            </tr>
+          )
+        } else if (item.includes("Sunday")) {
+          items.push(
+            <tr className="bg-info" key={index}>
+              <th>{item}</th>
+            </tr>
+          )
+        } else {
+          items.push(
+            <tr key={index}>
+              <td>{item}</td>
+            </tr>
+          )
+        }
       })
       console.log(items)
       ReactDOM.render(items, document.getElementById("schedule-items"))
@@ -34,13 +77,13 @@ function DropSchedule() {
 
   return (
     <>
-      <Row className="w-100 m-auto">
-        <Form.Label>Drop Schedule</Form.Label>
-        <Form.Control as="textarea" rows={3} onChange={catchText} />
+      <Table striped bordered hover variant="dark" className="m-auto">
+        <tbody id="schedule-items"></tbody>
+      </Table>
+
+      <Row className="m-auto">
+        <Form.Control as="textarea" className="drop-schedule bg-secondary" placeholder="drag schedule here (ctrl+a then drag whole page)" rows={1} width="1" onChange={catchText} />
       </Row>
-      <tbody>
-        <tr id="schedule-items"></tr>
-      </tbody>
     </>
   )
 }
