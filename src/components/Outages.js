0f01538c034874c58816e5389c99b6d9498dea26
Chris Steffes <c.steffes@live.ca>
2020-12-11 08:09:43 -0600

    ifram title fix

--- src/components/Outages.js
+++ src/components/Outages.js
@@ -3,7 +3,7 @@ import React from "react"
 function Outages() {
   return (
     <div>
-      <iframe src="https://eager-almeida-a52a97.netlify.app" id="serverurl" width="540" height="600" frameBorder="0"></iframe>
+      <iframe src="https://eager-almeida-a52a97.netlify.app" id="serverurl" width="540" height="600" title="outages-iframe" frameBorder="0"></iframe>
     </div>
   )
 }
