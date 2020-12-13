5101e030e368d94f6a1fe64945c55d8690325926
Chris Steffes <c.steffes@live.ca>
2020-12-13 16:48:41 -0600

    Persistent outages w/ LS + useEffect

--- src/contexts/OutageContext.js
+++ src/contexts/OutageContext.js
@@ -1,10 +1,16 @@
-import React, { createContext, useReducer } from "react"
+import React, { createContext, useReducer, useEffect } from "react"
 import { outageReducer } from "../reducers/OutageReducer"
 
 export const OutageContext = createContext()
 
 const OutageContextProvider = props => {
-  const [outages, dispatch] = useReducer(outageReducer, [])
+  const [outages, dispatch] = useReducer(outageReducer, [], () => {
+    const localData = localStorage.getItem("outages")
+    return localData ? JSON.parse(localData) : []
+  })
+  useEffect(() => {
+    localStorage.setItem("outages", JSON.stringify(outages))
+  }, [outages])
 
   return <OutageContext.Provider value={{ outages, dispatch }}>{props.children}</OutageContext.Provider>
 }
