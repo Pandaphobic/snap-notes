f79211c30ef008d92f4c26a9c42c8f5d153b13e3
Chris Steffes <c.steffes@live.ca>
2020-12-19 10:39:57 -0600

    persistent notes working

--- src/components/Notes/CallNotes.js
+++ src/components/Notes/CallNotes.js
@@ -7,11 +7,55 @@ const fontWeight = {
 
 const textarea = { fontWeight: "600", lineHeight: "1.20", fontSize: "14px", border: "none", margin: "0", padding: "5px" }
 
+let userID = () => {
+  let lsUserID = localStorage.getItem("userID")
+  return lsUserID ? lsUserID : ""
+}
+
+let phone = () => {
+  let lsphone = localStorage.getItem("phone")
+  return lsphone ? lsphone : ""
+}
+
+let prevTicket = () => {
+  let lsprevTicket = localStorage.getItem("prevTicket")
+  return lsprevTicket ? lsprevTicket : ""
+}
+
+let advisorCode = () => {
+  let lsadvisorCode = localStorage.getItem("advisorCode")
+  return lsadvisorCode ? lsadvisorCode : ""
+}
+let fullname = () => {
+  let lsfullname = localStorage.getItem("fullname")
+  return lsfullname ? lsfullname : ""
+}
+
+let quickNotes = () => {
+  let lsquickNotes = localStorage.getItem("quickNotes")
+  return lsquickNotes ? lsquickNotes : ""
+}
+
+function handleChange(e) {
+  console.log(e.target.name)
+  localStorage.setItem(e.target.name, e.target.value)
+}
+
+console.log(userID())
+
 function CallNotes() {
   let handleClear = () => {
     document.querySelectorAll("input").forEach(input => (input.value = ""))
     document.querySelector("textarea").value = ""
+    localStorage.removeItem("userID")
+    localStorage.removeItem("phone")
+    localStorage.removeItem("prevTicket")
+    localStorage.removeItem("advisorCode")
+    localStorage.removeItem("fullname")
+    localStorage.removeItem("quickNotes")
   }
+
+  let handleSave = () => {}
 
   let handleFlip = () => {
     let raw
@@ -45,22 +89,22 @@ function CallNotes() {
       <Row className="mb-2">
         <Col>
           <Form.Label>User ID</Form.Label>
-          <Form.Control name="callerUserID" style={fontWeight} type="text" placeholder="exampt1" />
+          <Form.Control onChange={e => handleChange(e)} name="userID" style={fontWeight} type="text" placeholder="exampt1" defaultValue={userID()} />
         </Col>
         <Col>
           <Form.Label>Phone Number</Form.Label>
-          <Form.Control name="callerPhone" style={fontWeight} type="text" placeholder="xxx-xxx-xxxx ext xxxx" />
+          <Form.Control onChange={e => handleChange(e)} name="phone" style={fontWeight} type="text" placeholder="xxx-xxx-xxxx ext xxxx" defaultValue={phone()} />
         </Col>
       </Row>
 
       <Row className="mb-2">
         <Col>
           <Form.Label>Previous Ticket</Form.Label>
-          <Form.Control name="callerPrevTicket" style={fontWeight} type="text" placeholder="INCXXXXXXX" />
+          <Form.Control onChange={e => handleChange(e)} name="prevTicket" style={fontWeight} type="text" placeholder="INCXXXXXXX" defaultValue={prevTicket()} />
         </Col>
         <Col>
           <Form.Label>Advisor Code</Form.Label>
-          <Form.Control name="advisorCode" style={fontWeight} type="text" placeholder="code" />
+          <Form.Control onChange={e => handleChange(e)} name="advisorCode" style={fontWeight} type="text" placeholder="code" defaultValue={advisorCode()} />
         </Col>
       </Row>
       {/* Full Name Section */}
@@ -73,18 +117,21 @@ function CallNotes() {
                 <i className="fas fa-sync-alt"></i>
               </Button>
             </InputGroup.Prepend>
-            <Form.Control name="callerName" id="fullname" style={fontWeight} type="text" placeholder="Lastname, Firstname" />
+            <Form.Control onChange={e => handleChange(e)} name="fullname" id="fullname" style={fontWeight} type="text" placeholder="Lastname, Firstname" defaultValue={fullname()} />
           </InputGroup>
         </Col>
       </Row>
 
       <Row className="w-100 m-auto">
         <Form.Label>Quick Notes</Form.Label>
-        <Form.Control name="quickNotes" style={textarea} as="textarea" rows={5} />
+        <Form.Control onChange={e => handleChange(e)} name="quickNotes" style={textarea} as="textarea" rows={5} defaultValue={quickNotes()} />
       </Row>
-      <Row className="mb-2">
-        <Button style={fontWeight} variant="outline-info" onClick={handleClear} className="btn-block m-3">
+      <Row className="mt-3">
+        <Button style={fontWeight} variant="outline-danger" onClick={handleClear} className="m-auto col-5">
           Clear All <i className="fas fa-eraser"></i>
+        </Button>
+        <Button style={fontWeight} variant="outline-info" onClick={handleSave} className="m-auto col-5">
+          Save All <i className="fas fa-save"></i>
         </Button>
       </Row>
     </Container>
